0e216c94c2304a7168c3e9daa747160b2c37a7de
NaijiaJin <43256871+NaijiaJin@users.noreply.github.com>
2020-02-02 13:51:15 -0500

    poop anim

---
+++ Assets/Scripts/BrearPoop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrearPoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject shit;
+    public GameObject positionobj;
+
+  public void ShitShow()
+    {
+        Instantiate(shit, positionobj.transform.position, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/Shitmove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shitmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rd;
+    public float initialSpeedx = 10.0f;
+    public float initialSpeedy = 10.0f;
+    void Start()
+    {
+        rd = this.GetComponent<Rigidbody2D>();
+        rd.velocity = new Vector2(initialSpeedx, initialSpeedy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
